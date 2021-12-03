156addf52ec41fac68bcc1081c1f9e3cd5e0f30f
daw2-Cardenas22 <71781302+EricCardenasMartin@users.noreply.github.com>
2021-12-03 14:46:44 +0000

    a

--- script.js
+++ script.js
@@ -1,96 +1,52 @@
-function suma(n1, n2){
-	return n1 + n2;
+var coches = []
+coches = newCoche("paco1","paco11","paco12");
+console.log(coches)
+console.log(coches['paco'])
+console.log(coches[0])
+
+function newCoche (name, brand, model){
+	// let cocheInfo = [ name= ['brand'=brand, 'model'=model]];
+	// cocheInfo[] = ;
+	// cocheInfo['model'] = model;
+
+	// let coche = []
+	// coche[name] = cocheInfo;
+
+	let cocheInfo = [];
+	cocheInfo['brand'] = brand;
+	cocheInfo['model'] = model;
+
+	let coche = []
+	coche[name] = cocheInfo;
+
+	return coche;
 }
 
-console.log(suma(2, 4));
-
-function comparar(a, b){
-	return a > b ? "mayor" : ( b > a ? "menor" : a*a);
+var coche = {
+	brand:"paco21",
+	model:"paco22"
 }
 
-console.log(comparar(2, 1));
+console.log(coche);
 
-function rndCoor(){
-	return [Math.random() * 360 - 180, Math.random() * 360 - 180];
+////////////////////////////////////////
+
+function newCoche (inputBrand, inputModel){
+	return {
+				brand:inputBrand,
+				model:inputModel
+			};
 }
 
-let coor = rndCoor();
-
-console.log(coor);
-
-function rndCoorArray(n){
+function newCocheArray(amount){
 	let array = [];
-
-	for (let i = 0; i < n; i++)
-		array.push(rndCoor());
-
-	return array;
-}
-
-let coorArray = rndCoorArray(4)
-
-console.log(coorArray);
-
-//////////////////////////////////
-
-function rndCoorTable(n){
-	let array = `<table border="1" style="text-align:center;">
-		<tr>
-			<th style="padding:5px;">Latitud</th>
-			<th style="padding:5px;">Longitud</th>
-		</tr>`;
-
-	for (let i = 0; i < n; i++){
-		let tempCoor = rndCoor();
-		
-		array += `<tr>
-			<td style="padding:5px;">${tempCoor[0]}</td>
-			<td style="padding:5px;">${tempCoor[1]}</td>
-		</tr>`;
-	}
-
-	array += `</table>`;
+	
+	for(let i = 0; i < amount; i++)
+		array.push(newCoche("Paco"+i+"1", "Paco"+i+"2"));
 
 	return array;
 }
 
-document.write(rndCoorTable(5));
-
-//////////////////////////////////
-
-function sayHelloTo(name, surname1, surname2){
-	return `Hola ${name} ${surname1} ${surname1}! :D`
+function cochesTable(cochesArray){
+	
 }
-
-document.write(`<p>${sayHelloTo("Domingo Antonio","Edjang","Moreno")}</p>`)
-
-/////////////////////////////////////
-
-function createCard(name, img){
-	return `<div class="card"><img src="${img}" alt="imagen de ${name}"><p>${name}</p></div> `
-}
-
-// document.write(createCard("patata", "https://ibericadepatatas.es/wp-content/uploads/2019/11/img-producto-patatas-voyager-iberica-de-patatas.jpg"));
-
-////////////////////////////////
-
-function createCards(cardsInfo){
-
-	let returnMe = "";
-
-	returnMe += `<div style="display:flex;flex-wrap:wrap;justify-content:space-around;gap: 30px;">`;
-
-	for(let cardInfo in cardsInfo)
-		returnMe += createCard(cardsInfo[cardInfo][0], cardsInfo[cardInfo][1]);
-
-	returnMe += `</div>`;
-
-	return returnMe;
-}
-
-var dishes = [	["patata", "https://ibericadepatatas.es/wp-content/uploads/2019/11/img-producto-patatas-voyager-iberica-de-patatas.jpg"],
-				["patata", "https://ibericadepatatas.es/wp-content/uploads/2019/11/img-producto-patatas-voyager-iberica-de-patatas.jpg"],
-				["patata", "https://ibericadepatatas.es/wp-content/uploads/2019/11/img-producto-patatas-voyager-iberica-de-patatas.jpg"],
-				["patata", "https://ibericadepatatas.es/wp-content/uploads/2019/11/img-producto-patatas-voyager-iberica-de-patatas.jpg"]];
-
-document.write(createCards(dishes));
